dad7b2de5e99a623abd212ec5ec5f0eb0b0c003f
syshindev <syshindev@gmail.com>
2026-03-18 14:17:49 +0900

    feat: update App with dashboard layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import VideoStream from "./VideoStream";
+import EventLog from "./EventLog";
+
+function App() {
+  return (
+    <div>
+      <h1>Edge AI Surveillance Dashboard</h1>
+      <VideoStream />
+      <EventLog />
+    </div>
+  );
+}
+
+export default App;
